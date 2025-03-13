ace16e17eaaec9e2150a3e7926e39c928f40070c
ImXiaoYe <2423336405@qq.com>
2025-03-13 17:07:57 +0800

    1

--- 明文源码.js
+++ 明文源码.js
@@ -25,7 +25,7 @@ let 伪装网页 = "";
 
 // 网页入口
 export default {
-  async fetch(访问请求, env) {
+  async fetch(访问请求, env, hostName) {
     订阅路径 = env.SUB_PATH ?? 订阅路径;
     我的UUID = env.SUB_UUID ?? 我的UUID;
     默认节点名称 = env.SUB_NAME ?? 默认节点名称;
