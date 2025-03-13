c9dd29587c3f94247e15e43c318246237f846a02
能大 <139523852+ImLTHQ@users.noreply.github.com>
2025-03-13 22:27:20 +0800

    更新 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -66,7 +66,7 @@ export default {
             v2ray: v2ray配置文件,
             clash: clash配置文件,
             "sing-box": singbox配置文件,
-            default: singbox配置文件,
+            default: 生成提示界面,
             //default: 生成提示界面,
           };
           const 工具 = Object.keys(配置生成器).find((工具) => 用户代理.includes(工具));
