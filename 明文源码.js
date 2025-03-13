a08f9832c9deff4860aa3728c199a33e6f242b79
ImXiaoYe <2423336405@qq.com>
2025-03-13 17:25:57 +0800

    小的修改

--- 明文源码.js
+++ 明文源码.js
@@ -367,10 +367,11 @@ function 处理优选列表(优选列表, hostName) {
 }
 
 function v2ray配置文件(hostName) {
+  const path = encodeURIComponent("/?ed=2560");
   const 节点列表 = 处理优选列表(优选列表, hostName);
   const 配置内容 = 节点列表
     .map(({ 地址, 端口, 节点名字 }) => {
-      return `vless://${我的UUID}@${地址}:${端口}?encryption=none&security=tls&sni=${hostName}&fp=chrome&type=ws&host=${hostName}&path=/?ed=2560#${节点名字}`;
+      return `vless://${我的UUID}@${地址}:${端口}?encryption=none&security=tls&sni=${hostName}&fp=chrome&type=ws&host=${hostName}&path=${path}#${节点名字}`;
     })
     .join("\n");
 
