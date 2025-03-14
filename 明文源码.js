2d5783dd1dea5d1871c8e95bd51c816ed9fe0dce
ImXiaoYe <2423336405@qq.com>
2025-03-14 18:05:45 +0800

    小的修改

--- 明文源码.js
+++ 明文源码.js
@@ -504,78 +504,6 @@ function singbox配置文件(hostName) {
   "level": "info",
   "timestamp": true
 },
-"dns": {
-  "servers": [
-    {
-      "tag": "dns_proxy",
-      "address": "tls://1.1.1.1",
-      "address_resolver": "dns_resolver"
-    },
-    {
-      "tag": "dns_direct",
-      "address": "h3://dns.alidns.com/dns-query",
-      "address_resolver": "dns_resolver",
-      "detour": "DIRECT"
-    },
-    {
-      "tag": "dns_fakeip",
-      "address": "fakeip"
-    },
-    {
-      "tag": "dns_resolver",
-      "address": "223.5.5.5",
-      "detour": "DIRECT"
-    },
-    {
-      "tag": "block",
-      "address": "rcode://success"
-    }
-  ],
-  "rules": [
-    {
-      "outbound": [
-        "any"
-      ],
-      "server": "dns_resolver"
-    },
-    {
-      "geosite": [
-        "category-ads-all"
-      ],
-      "server": "dns_block",
-      "disable_cache": true
-    },
-    {
-      "geosite": [
-        "geolocation-!cn"
-      ],
-      "query_type": [
-        "A",
-        "AAAA"
-      ],
-      "server": "dns_fakeip"
-    },
-    {
-      "geosite": [
-        "geolocation-!cn"
-      ],
-      "server": "dns_proxy"
-    }
-  ],
-  "final": "dns_direct",
-  "independent_cache": true,
-  "fakeip": {
-    "enabled": true,
-    "inet4_range": "198.18.0.0/15"
-  }
-},
-"ntp": {
-  "enabled": true,
-  "server": "time.apple.com",
-  "server_port": 123,
-  "interval": "30m",
-  "detour": "DIRECT"
-},
 "inbounds": [
   {
     "type": "mixed",
@@ -601,10 +529,6 @@ function singbox配置文件(hostName) {
   {
     "type": "block",
     "tag": "REJECT"
-  },
-  {
-    "type": "dns",
-    "tag": "dns-out"
   },${节点配置},
   {
     "type": "selector",
@@ -636,24 +560,11 @@ function singbox配置文件(hostName) {
 "route": {
   "rules": [
     {
-      "clash_mode": "Global",
-      "outbound": "GLOBAL"
-    },
-    {
-      "clash_mode": "Direct",
-      "outbound": "DIRECT"
-    },
-    {
-      "protocol": "dns",
-      "outbound": "dns-out"
-    },
-    {
-      "geoip": "lan",
-      "outbound": "DIRECT"
-    },
-    {
       "geoip": "cn",
       "outbound": "DIRECT"
+    },
+    {
+      "outbound": "🚀 节点选择"
     }
   ],
   "auto_detect_interface": true,
